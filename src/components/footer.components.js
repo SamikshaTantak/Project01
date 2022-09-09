d614ecf98c11b0d954b0d70a56e370c1a180f738
SamikshaTantak <samikshatantak44@gmail.com>
2022-09-09 19:22:53 +0530

    updated

---
+++ src/components/footer.components.js
@@ -0,0 +1,23 @@
+const Footer = () => {
+    return(<>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+    <p class="fs-6"> Need Help?</p><br></br>
+    <p>+91 11 57296440   +91 23 76286936</p>
+    </div>
+    <div class="col">
+    <p class="fs-6"> Developed By</p><br></br>
+    <p> Samiksha Tantak   Saniya Shaikh</p><br></br>
+    <p> Pranali Pawar   Akshada Patil</p>
+    </div>
+    <div class="col">
+      <p> Copyright- Travlendia|2022</p>
+    </div>
+  </div>
+</div>
+
+    </>)
+}
+export default Footer;
---
+++ src/components/section.components.js
@@ -0,0 +1,16 @@
+const section = () => {
+    return(<>
+
+    <div className="container">
+  <div className="row">
+    <div className="col">
+  
+    </div>
+    <div class="col">
+    </div>
+  </div>
+</div>
+
+    </>)
+}
+export default section;
